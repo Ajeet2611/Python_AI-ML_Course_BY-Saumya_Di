12502fa1f76af6c5f8cd9b69b6adb86baa7524f6
ajeetkumar <ajeetkumar@AJEET>
2025-12-02 00:42:53 +0530

    implicit convertion

---
+++ Chapter 2/implicit.py
@@ -0,0 +1,9 @@
+x =5 #int
+y = 3.5 #float
+z = "Hello" #string
+print(type(x))
+print(type(y))
+print(type(z))
+sum = x + y
+print("Sum of", x, "and", y, "is:", sum) #float
+print("Data type of sum is:", type(sum)) #float
