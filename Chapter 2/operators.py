84793b3e1c60c65d788cf73d2039f04e1c0946c7
ajeetkumar <ajeetkumar@AJEET>
2025-12-02 01:10:03 +0530

    operators in python

---
+++ Chapter 2/operators.py
@@ -0,0 +1,53 @@
+#arithmetic operators in python
+x = 10
+y= 3
+sum = x+y
+diff = x - y
+prod = x * y
+quotient = x / y
+floor_div = x // y
+modulus = x % y
+exponent = x ** y
+print("Sum:", sum)
+print("Difference:", diff)
+print("Product:", prod)
+print("Quotient:", quotient)
+print("Floor Division:", floor_div)
+print("Modulus:", modulus)
+print("Exponentiation:", exponent)
+# --- IGNORE ---
+#Comparison operators in python
+
+print("x equal to y:", x == y)
+print("x not equal to y:", x != y)
+print("x greater than y:", x > y)
+print("x less than y:", x < y)
+print("x greater than or equal to y:", x >= y)
+print("x less than or equal to y:", x <= y)
+# --- IGNORE ---
+#Logical operators in python
+a = True
+b = False
+print("a and b:", a and b)#False
+print("a or b:", a or b) #True
+print("not a:", not a) #False
+print("not b:", not b)#True
+# --- IGNORE ---
+
+#Assignment operators in python
+c = 5
+print("Initial value of c:", c)
+c += 3
+print("After c += 3, c =", c)
+c -= 2
+print("After c -= 2, c =", c)
+c *= 4
+print("After c *= 4, c =", c)
+c /= 3
+print("After c /= 3, c =", c)
+c %= 5
+print("After c %= 5, c =", c)
+c **= 2
+print("After c **= 2, c =", c)
+# --- IGNORE ---
+
