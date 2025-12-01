7417af3b6bae58169bdd9b36ab7fdf5e23b6a0a0
ajeetkumar <ajeetkumar@AJEET>
2025-12-01 23:14:04 +0530

    Data type

---
+++ Chapter 2/dataTypes.py
@@ -0,0 +1,16 @@
+food = "Samosa"
+age = 21
+price = 25.5
+name = "Ajeet"
+print(type(food))
+print(type(age))
+print(type(price))
+print(type(name))
+print("Food Item:", food)
+print("Age:", age)
+print("Price:", price)
+print("Name:", name)
+print("Data type of food is:", type(food))
+print("Data type of age is:", type(age))
+print("Data type of price is:", type(price))
+print("Data type of name is:", type(name))
