119db6f27dafb66e88e22df3baa9c7b251a20040
ajeetkumar <ajeetkumar@AJEET>
2025-12-02 00:48:37 +0530

    implicit casting

---
+++ Chapter 2/explicit.py
@@ -0,0 +1,10 @@
+#take num  as input 
+#  convert  to float and print both original and converted data types
+#value with their data types
+
+num = input("Enter a number: ")
+convertedValue = float(num)
+print("Original value:", num)
+print("Data type of original value is:", type(num))
+print("Converted value:", convertedValue)   
+print("Data type of converted value is:", type(convertedValue))
