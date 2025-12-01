1dc27a40471b8ced307173b6c507ebc60205f66d
ajeetkumar <ajeetkumar@AJEET>
2025-12-01 18:36:53 +0530

    redius print

--- Chapter 1/Assigment2.py
+++ Chapter 1/Assigment2.py
@@ -2,5 +2,6 @@
 
 diameter = float(input("Enter the diameter of the circle: "))
 redius = diameter / 2
+print ("Redius of circle is: ", redius)
 areaOf_circle = 3.14 * (redius**2)
 print("Area of circle is: ", areaOf_circle)
