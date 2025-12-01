2c79efc685c96c99b572b3fc6f7eb88d0293104a
ajeetkumar <ajeetkumar@AJEET>
2025-12-01 18:25:51 +0530

    change area of _Crircle formula

--- Chapter 1/Assigment2.py
+++ Chapter 1/Assigment2.py
@@ -2,5 +2,5 @@
 
 diameter = float(input("Enter the diameter of the circle: "))
 redius = diameter / 2
-areaOf_circle = (22/7*redius**2)
+areaOf_circle = 3.14 * (redius**2)
 print("Area of circle is: ", areaOf_circle)
