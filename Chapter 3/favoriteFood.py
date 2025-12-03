4dfbe1179ee4a7f72452c3dc19786890ce4a4477
ajeetkumar <ajeetkumar@AJEET>
2025-12-04 00:19:55 +0530

    nagetive indexing

--- Chapter 3/favoriteFood.py
+++ Chapter 3/favoriteFood.py
@@ -1,3 +1,6 @@
 favoriteFood="Pizza"
 print("my favorite food print middle 3 letters:",favoriteFood[1:4])
 print("my favorite food print last 2 letters:",favoriteFood[3:5])
+#nagetive indexing
+print("my favoraite food print middele 3 letters using negative indexing:",favoriteFood[-4:-1])
+print("my favorite food print last 2 letters using negative indexing:",favoriteFood[-2:])
