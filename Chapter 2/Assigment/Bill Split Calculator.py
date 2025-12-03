f7a980df4f4be4d5578b8578fdb72bd6323c73c0
ajeetkumar <ajeetkumar@AJEET>
2025-12-03 18:23:34 +0530

    Bill Split Calculators

---
+++ Chapter 2/Assigment/Bill Split Calculator.py
@@ -0,0 +1,5 @@
+totalBill = float(input("Enter the total bill amount: "))
+numberOfFriends = int(input("Enter the number of friends to split the bill: "))
+each_Will_Pay = totalBill / numberOfFriends
+print("Each friend should pay: ", each_Will_Pay)
+# Simple Bill Split Calculator
