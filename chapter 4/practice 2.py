32567a628e2c8748795618dfce635535394655d4
ajeetkumar <ajeetkumar@AJEET>
2025-12-04 22:39:30 +0530

    practice question 2

---
+++ chapter 4/practice 2.py
@@ -0,0 +1,8 @@
+#user input 3 favorite food and store in list and print all food and length of list
+food1 = input("enter your first favorite food: ")
+food2 = input("enter your second favorite food: ")
+food3 = input("enter your third favorite food: ")
+list = [food1,food2,food3]
+print("your favorite food are : ",list)
+print("lenth of list = ",len(list))
+
