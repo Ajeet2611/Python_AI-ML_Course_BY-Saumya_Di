cf72d1db43a5978ad644a1341904892deefd3a33
ajeetkumar <ajeetkumar@AJEET>
2025-12-04 20:57:34 +0530

    find number is positive , negetive or zero

---
+++ chapter 4/practice1.py
@@ -0,0 +1,15 @@
+#take input and print positive if num is greater than zero  so on than zero else negative else zero
+num = int(input("enter a number: "))
+
+if (num > 0):
+    print("positive")
+elif (num<0):
+    print("negative")
+else:
+    print("zero")
+
+default_message = "my code end here"
+print(default_message)
+
+
+            
