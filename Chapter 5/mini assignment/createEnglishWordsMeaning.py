aeb14845c52906de1bb7468267e4dc7d30ac38ae
ajeetkumar <ajeetkumar@AJEET>
2025-12-07 20:18:55 +0530

    storing meaning of 3 English Words

---
+++ Chapter 5/mini assignment/createEnglishWordsMeaning.py
@@ -0,0 +1,17 @@
+#create a dictionary storing meaning of 3 English Words 
+words_meaning = {
+    "Happy": "Feeling or showing pleasure; khush rehna",
+    "Brave": "Ready to face danger or pain; bahadur",
+    "Honest": "Truthful and sincere; imaandaar"
+
+}
+# Print the type of the dictionary
+print("Program type is :",type(words_meaning))
+
+# Print the meaning of each word
+print("meaning of Happy is : " ,words_meaning["Happy"])
+print("meaning of Brave is : " , words_meaning["Brave"])
+print("meaning of Honest is : ",words_meaning["Honest"])
+
+#print the length of the dictionary
+print("length of dictionary is : ",len(words_meaning))
