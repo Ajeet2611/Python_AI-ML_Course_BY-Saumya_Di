725e9886175b751be364e4aa2553a96b2f837992
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 01:27:45 +0530

    print table

---
+++ Chapter 6/practice 7.py
@@ -0,0 +1,10 @@
+#printing table
+
+num = int(input("Please choose any number of print table : "))
+print("Table of num : ",num)
+i = 1
+while i<=10 :
+   
+    print(num ,"*" , i ,"=", i*num)
+
+    i = i+1
