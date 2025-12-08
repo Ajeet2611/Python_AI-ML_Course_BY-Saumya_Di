0d26bb3986dc94361be2bd744566b07662d73c5c
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 13:11:12 +0530

    create tuple and Access element using for loop

---
+++ Chapter 6/practice15.py
@@ -0,0 +1,5 @@
+# Ajeet Has created a tuple of countries she has already traveled to . write a Python Program to print each country using a for loop.
+traveled_Countries_create_tuple = ("india","Afghanistan", "Canada", "France", "Japan", "Brazi")
+
+for country in traveled_Countries_create_tuple:
+    print(country)
