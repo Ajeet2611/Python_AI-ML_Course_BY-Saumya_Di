bf7bbba360a90f256d782add22778f2ed3e9aa50
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 19:34:47 +0530

    print Welcome Massage

---
+++ Chapter 7/Practice Question/question 1.py
@@ -0,0 +1,7 @@
+#write a Function named Welcome_massege () That print "welcome to python Programming!"three times.
+def Welcome_massage():
+    print("welcome to python Programming!")
+
+Welcome_massage ()
+Welcome_massage ()
+Welcome_massage ()
