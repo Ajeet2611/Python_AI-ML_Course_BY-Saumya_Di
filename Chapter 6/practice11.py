fded1482f013982612463f42b925e408fb7115c1
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 12:30:54 +0530

    table printing code

--- Chapter 6/practice11.py
+++ Chapter 6/practice11.py
@@ -1,1 +1,8 @@
 #Write a program that prints the multiplication table of any number entered the user using a for loop.
+
+num = int (input("Please enter a number : "))
+
+print("Printing a table ",num,"is :")
+
+for i in range (1 , 11 ,1):
+    print(num ,"x",i," = ",num*i)
